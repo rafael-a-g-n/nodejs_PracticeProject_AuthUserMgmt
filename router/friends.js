9e2739ac76cb8feeb5afd68c5349c7d49bc68eda
rafael-a-g-n <ragn.ryzen@gmail.com>
2025-11-27 16:03:49 +0000

    Implemented GET endpoint for friend by email ID

--- router/friends.js
+++ router/friends.js
@@ -27,8 +27,8 @@ router.get("/", (req, res) => {
 
 // GET by specific ID request: Retrieve a single friend with email ID
 router.get("/:email", (req, res) => {
-  // Update the code here
-  res.send("Yet to be implemented"); //This line is to be replaced with actual return value
+  const email = req.params.email;
+  res.send(friends[email]);
 });
 
 // POST request: Add a new friend
